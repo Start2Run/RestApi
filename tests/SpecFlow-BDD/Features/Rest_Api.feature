b213736f13eb75dd37e93f5b7f09241d051085ff
Start2Run <MihaiOlaru@protonmail.ch>
2020-11-21 20:56:40 -0500

    Add BDD scenario for the REST API endpoint

---
+++ tests/SpecFlow-BDD/Features/Rest_Api.feature
@@ -0,0 +1,13 @@
+﻿Feature: Query Rest Api endpoint
+![REST_API]
+	In order to monitor the outside temperature
+	I *want* to automatically get the **temperature** from a ***REST API endpoint***
+
+Link to a feature: [REST_API](SpecFlow-BDD/Features/Rest_Api.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: GET Api request should return the expected JSON data model
+	Given a valid endpoint address is configured in the application configuration file 
+	When the request is sent to a Rest Api endpoint
+	Then the returned body data should have the format JSON data model
---
+++ tests/SpecFlow-BDD/Steps/QueryRestApiEndpointSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowBDD.Steps
+{
+    [Binding]
+    public class QueryRestApiEndpointSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public QueryRestApiEndpointSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"a valid endpoint address is configured in the application configuration file")]
+        public void GivenAValidEndpointAddressIsConfiguredInTheApplicationConfigurationFile()
+        {
+            _scenarioContext.Pending();
+        }
+        
+        [When(@"the request is sent to a Rest Api endpoint")]
+        public void WhenTheRequestIsSentToTheRestApiEndpoint()
+        {
+            _scenarioContext.Pending();
+        }
+        
+        [Then(@"the returned body data should have the format JSON data model")]
+        public void ThenTheReturnedBodyDataShouldHaveTheFormatJSONDataModel()
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
